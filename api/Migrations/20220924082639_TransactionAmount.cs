58c8d64bd47a5625f80cc76b2a9b72445a058f41
ajkarell <111962031+ajkarell@users.noreply.github.com>
2022-09-24 11:26:56 +0300

    api: refactor transaction

---
+++ api/Migrations/20220924082639_TransactionAmount.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecomaniac.Api.Migrations
+{
+    public partial class TransactionAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ChangeInBalance",
+                table: "Transactions",
+                newName: "Amount");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "Transactions",
+                newName: "ChangeInBalance");
+        }
+    }
+}
--- api/Models/Transaction.cs
+++ api/Models/Transaction.cs
@@ -5,6 +5,6 @@ public class Transaction
     [Key]
     public int Id { get; set; }
     public DateTimeOffset Date { get; set; }
-    public decimal ChangeInBalance { get; set; }
+    public decimal Amount { get; set; }
     public string Description { get; set; }
 }
