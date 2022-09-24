4cd43874d7c6fc7f1d398f6e8068507106e713a9
ajkarell <111962031+ajkarell@users.noreply.github.com>
2022-09-24 11:14:08 +0300

    api: replace expense with transaction model

--- api/ApplicationDbContext.cs
+++ api/ApplicationDbContext.cs
@@ -1,8 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<Expense> Expenses => Set<Expense>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
 
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
@@ -11,7 +12,29 @@ public class ApplicationDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Expense>(builder =>
+        if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+        {
+            // SQLite does not have proper support for DateTimeOffset via Entity Framework Core, see the limitations
+            // here: https://docs.microsoft.com/en-us/ef/core/providers/sqlite/limitations#query-limitations
+            // To work around this, when the Sqlite database provider is used, all model properties of type DateTimeOffset
+            // use the DateTimeOffsetToBinaryConverter
+            // Based on: https://github.com/aspnet/EntityFrameworkCore/issues/10784#issuecomment-415769754
+            // This only supports millisecond precision, but should be sufficient for most use cases.
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(DateTimeOffset)
+                                                                            || p.PropertyType == typeof(DateTimeOffset?));
+                foreach (var property in properties)
+                {
+                    modelBuilder
+                        .Entity(entityType.Name)
+                        .Property(property.Name)
+                        .HasConversion(new DateTimeOffsetToBinaryConverter());
+                }
+            }
+        }
+
+        modelBuilder.Entity<Transaction>(builder =>
         {
             builder.HasKey(x => x.Id);
         });
--- api/Models/Transaction.cs
+++ api/Models/Transaction.cs
@@ -1,9 +1,10 @@
 using System.ComponentModel.DataAnnotations;
 
-public class Expense
+public class Transaction
 {
     [Key]
     public int Id { get; set; }
     public DateTimeOffset Date { get; set; }
     public decimal ChangeInBalance { get; set; }
+    public string Description { get; set; }
 }
