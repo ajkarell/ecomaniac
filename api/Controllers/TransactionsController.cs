5fbc36d63c695df4767f0b8f26644c95335d09f1
ajkarell <111962031+ajkarell@users.noreply.github.com>
2022-09-25 13:23:34 +0300

    api: add delete endpoint

--- api/Controllers/TransactionsController.cs
+++ api/Controllers/TransactionsController.cs
@@ -18,7 +18,7 @@ public class TransactionsController : ControllerBase
     public async Task<ActionResult<IEnumerable<Transaction>>> GetAllTransactions()
     {
         var transactions = await _db.Transactions
-            .OrderBy(x => x.Date)
+            .OrderByDescending(x => x.Date)
             .ToListAsync();
 
         if (!transactions.Any())
@@ -42,9 +42,29 @@ public class TransactionsController : ControllerBase
     [HttpPost("transactions")]
     public async Task<ActionResult<Transaction>> PostTransaction(Transaction transaction)
     {
+        if (transaction.Amount == 0)
+        {
+            return BadRequest("Transaction amount cannot be zero.");
+        }
+
+        if (transaction.Date > DateTimeOffset.Now)
+        {
+            return BadRequest("Transaction date cannot be in the future.");
+        }
+
         _db.Add(transaction);
         await _db.SaveChangesAsync();
 
         return CreatedAtAction(nameof(GetTransaction), new { id = transaction.Id }, transaction);
+    }
+
+    [HttpDelete("transactions")]
+    public async Task<ActionResult> DeleteTransaction(int id)
+    {
+        var transaction = new Transaction { Id = id };
+        _db.Remove(transaction);
+        await _db.SaveChangesAsync();
+
+        return NoContent();
     }
 }
